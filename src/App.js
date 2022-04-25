4574e8ae61f7b49eae47cba694c5f1fefe13a1ad
Unbreaks <51781170+Unbreaks@users.noreply.github.com>
2022-04-25 23:19:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import { useState, createContext, useEffect } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import GameOver from "./components/GameOver";
+import Keyboard from "./components/Keyboard";
+import { boardDefault, generateWordSet } from "./Words";
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrentAttempt] = useState({
+    attempt: 0,
+    letterPos: 0,
+  });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetter] = useState([]);
+  const [correctWord, setCorrectWord] = useState("");
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false,
+  });
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+  }, []);
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+
+    const currWord = board[currAttempt.attempt].join('');
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrentAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      alert("Word Not Found");
+    }
+
+    if (currWord.toLowerCase() === correctWord) {
+      setGameOver({ gameOver: true, guessedWord: true });
+      return;
+    }
+    if (currAttempt.attempt === 5) {
+      setGameOver({ gameOver: true, guessedWord: false });
+      return;
+    }
+  };
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currAttempt,
+      letterPos: currAttempt.letterPos + 1,
+    });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currAttempt,
+      letterPos: currAttempt.letterPos - 1,
+    });
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrentAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          correctWord,
+          setDisabledLetter,
+          disabledLetters,
+          gameOver,
+        }}
+      >
+        <div className="game">
+          <Board />
+          {correctWord}
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
